d8309e89a561914d094aa577e90da949c5570cb0
Instackler <izmestiev.kapr.2@yandex.ru>
2018-12-30 00:54:51 +0400

    16512

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include <SFML/Graphics.hpp>
+#include <entity.h>
+
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Project255");
+
+	// create an array of 3 vertices that define a triangle primitive
+	sf::VertexArray triangle(sf::Triangles, 3);
+
+	// define the position of the triangle's points
+	triangle[0].position = sf::Vector2f(10.f, 10.f);
+	triangle[1].position = sf::Vector2f(100.f, 10.f);
+	triangle[2].position = sf::Vector2f(100.f, 100.f);
+
+	// define the color of the triangle's points
+	triangle[0].color = sf::Color::Yellow;
+	triangle[1].color = sf::Color::Yellow;
+	triangle[2].color = sf::Color::Yellow;
+
+	sf::Transform t1 = sf::Transform::Identity;
+
+	// a rotation transform
+	sf::Transform t2;
+	t2.rotate(16.f);
 
 	while (window.isOpen())
 	{
@@ -15,8 +34,8 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		window.draw(shape);
+		window.clear(sf::Color(30, 30, 30, 255));
+		window.draw(triangle, t2);
 		window.display();
 	}
 
